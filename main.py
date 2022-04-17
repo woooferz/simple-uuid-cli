cd9f3631363e508eff22945f8f7796f66196fab9
woooferz <85282355+woooferz@users.noreply.github.com>
2022-04-17 11:00:49 +1000

    Starting Project...

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# ===============================
+# Author: Wooferz
+# Year: 2022
+# ===============================
+
+import uuid
+
+print("\n")
+print("Simple UUID CLI  Copyright (C) 2022 Wooferz")
+print("This program comes with ABSOLUTELY NO WARRANTY")
+print("This is free software, and you are welcome to redistribute it")
+print("under certain conditions")
+print("\n\n\n\n\n")
